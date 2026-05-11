77ab19d4b3d11305406d3cbfcd0dbac19ba35176
KR-Rai <kaydon.rai@mail.bcu.ac.uk>
2026-05-11 21:26:40 +0100

    Update #12
    Raytracing textures completed
    
    UV clamp had to be implemented (through thorough investigation of what the issue was i figured out how to incorporate a UV clamp into the lambertian shader)

--- Coursework/Raytracer/TexturedLambertianShader.hpp
+++ Coursework/Raytracer/TexturedLambertianShader.hpp
@@ -8,49 +8,64 @@
 class TexturedLambertianShader : public Shader
 {
 private:
-	const std::vector<uint8_t>* albedoTexture_;
-	const int texWidth_, texHeight_;
-	bool shadowTest_;
+    const std::vector<uint8_t>* albedoTexture_;
+    const int texWidth_, texHeight_;
+    bool shadowTest_;
 public:
-	TexturedLambertianShader(const std::vector<uint8_t>* albedoTexture, int texWidth, int texHeight, bool shadowTest=true)
-		:shadowTest_(shadowTest), albedoTexture_(albedoTexture),
-		texWidth_(texWidth), texHeight_(texHeight)
-	{}
+    TexturedLambertianShader(const std::vector<uint8_t>* albedoTexture, int texWidth, int texHeight, bool shadowTest = true)
+        :shadowTest_(shadowTest), albedoTexture_(albedoTexture),
+        texWidth_(texWidth), texHeight_(texHeight)
+    {
+    }
 
-	virtual Eigen::Vector3f getColor(const HitInfo& hitInfo, 
-		const Renderable* scene, 
-		const std::vector<std::unique_ptr<Light>>& lights,
-		const Eigen::Vector3f& ambientLight,
-		int currBounceCount,
-		const int maxBounces) const
-	{
-		Eigen::Vector3f albedo;
+    virtual Eigen::Vector3f getColor(const HitInfo& hitInfo,
+        const Renderable* scene,
+        const std::vector<std::unique_ptr<Light>>& lights,
+        const Eigen::Vector3f& ambientLight,
+        int currBounceCount,
+        const int maxBounces) const
+    {
+        Eigen::Vector3f albedo;
 
-		Eigen::Vector2f tex = hitInfo.texCoords;
-		int pixX = static_cast<int>(tex.x() * texWidth_);
-		int pixY = static_cast<int>((1.f - tex.y()) * texHeight_);
-		pixX = std::max(pixX, 0);
-		pixY = std::max(pixY, 0);
-		pixX = std::min(pixX, texWidth_);
-		pixY = std::min(pixY, texHeight_);
+        if (!albedoTexture_ || albedoTexture_->empty() || texWidth_ <= 0 || texHeight_ <= 0)
+            return Eigen::Vector3f(1, 0, 1);
 
-		albedo.x() = static_cast<float>((*albedoTexture_)[(pixX + texWidth_ * pixY) * 4 + 0]) / 255.f;
-		albedo.y() = static_cast<float>((*albedoTexture_)[(pixX + texWidth_*pixY)*4 + 1]) / 255.f;
-		albedo.z() = static_cast<float>((*albedoTexture_)[(pixX + texWidth_*pixY)*4 + 2]) / 255.f;
+        Eigen::Vector2f tex = hitInfo.texCoords;
 
-		Eigen::Vector3f color = coefftWiseMul(albedo, ambientLight);
+        // ===== UV CLAMP =====
+        tex.x() = std::max(0.0f, std::min(0.9999f, tex.x()));
+        tex.y() = std::max(0.0f, std::min(0.9999f, tex.y()));
 
-		for (auto& light : lights) {
-			if (shadowTest_) {
-				if (!light->visibilityCheck(hitInfo.location, scene))
-					continue;
-			}
-			Eigen::Vector3f lightVec = light->getVecToLight(hitInfo.location);
-			float dotProd = std::max(lightVec.dot(hitInfo.normal), 0.f);
-			color += dotProd * coefftWiseMul(light->getIntensity(hitInfo.location), albedo);
-		}
+        int pixX = static_cast<int>(tex.x() * texWidth_);
+        int pixY = static_cast<int>((1.f - tex.y()) * texHeight_);
 
-		return color;
-	}
+        pixX = std::max(pixX, 0);
+        pixY = std::max(pixY, 0);
+
+        pixX = std::min(pixX, texWidth_ - 1);
+        pixY = std::min(pixY, texHeight_ - 1);
+
+
+        int idx = (pixX + texWidth_ * pixY) * 4;
+        if (idx + 2 >= albedoTexture_->size())
+            return Eigen::Vector3f(0, 1, 0);
+
+        albedo.x() = static_cast<float>((*albedoTexture_)[idx + 0]) / 255.f;
+        albedo.y() = static_cast<float>((*albedoTexture_)[idx + 1]) / 255.f;
+        albedo.z() = static_cast<float>((*albedoTexture_)[idx + 2]) / 255.f;
+
+        Eigen::Vector3f color = coefftWiseMul(albedo, ambientLight);
+
+        for (auto& light : lights) {
+            if (shadowTest_) {
+                if (!light->visibilityCheck(hitInfo.location, scene))
+                    continue;
+            }
+            Eigen::Vector3f lightVec = light->getVecToLight(hitInfo.location);
+            float dotProd = std::max(lightVec.dot(hitInfo.normal), 0.f);
+            color += dotProd * coefftWiseMul(light->getIntensity(hitInfo.location), albedo);
+        }
+
+        return color;
+    }
 };
-
--- Coursework/Raytracer/main.cpp
+++ Coursework/Raytracer/main.cpp
@@ -28,9 +28,9 @@
 /// </summary>
 nlohmann::json loadConfig(const std::string& filename)
 {
-	std::ifstream configStream(filename);
-	nlohmann::json config = nlohmann::json::parse(configStream);
-	return config;
+    std::ifstream configStream(filename);
+    nlohmann::json config = nlohmann::json::parse(configStream);
+    return config;
 }
 
 /// <summary>
@@ -39,169 +39,191 @@ nlohmann::json loadConfig(const std::string& filename)
 /// </summary>
 Eigen::Vector3f loadVec3FromConfig(const nlohmann::json& config)
 {
-	return Eigen::Vector3f(config[0], config[1], config[2]);
+    return Eigen::Vector3f(config[0], config[1], config[2]);
 }
 
 int main(int argc, char* argv[]) {
 
-	// *** Load the config file ***
-	auto config = loadConfig("../config/config.json");
+    // *** Load the config file ***
+    auto config = loadConfig("../config/config.json");
 
-	const int pixHeight = config["pixHeight"], pixWidth = config["pixWidth"];
-	const int nChannels = 4;
+    const int pixHeight = config["pixHeight"], pixWidth = config["pixWidth"];
+    const int nChannels = 4;
 
-	// *** Set up camera and output image ***
-	Camera cam(
-		loadVec3FromConfig(config["cameraPos"]),
-		loadVec3FromConfig(config["cameraForward"]),
-		loadVec3FromConfig(config["cameraUp"]),
-		pixWidth, pixHeight,
-		config["cameraFov"]);
+    // *** Set up camera and output image ***
+    Camera cam(
+        loadVec3FromConfig(config["cameraPos"]),
+        loadVec3FromConfig(config["cameraForward"]),
+        loadVec3FromConfig(config["cameraUp"]),
+        pixWidth, pixHeight,
+        config["cameraFov"]);
 
-	std::vector<uint8_t> outImage(pixHeight * pixWidth * nChannels);
+    std::vector<uint8_t> outImage(pixHeight * pixWidth * nChannels);
 
-	Eigen::Vector3f
-		red(1.f, 0.f, 0.f),
-		blue(0.f, 0.f, 1.f),
-		aqua(0.f, .8f, .8f),
-		lavender(178.f / 255.f, 164.f / 255.f, 212.f / 255.f);
+    Eigen::Vector3f
+        red(1.f, 0.f, 0.f),
+        blue(0.f, 0.f, 1.f),
+        aqua(0.f, .8f, .8f),
+        lavender(178.f / 255.f, 164.f / 255.f, 212.f / 255.f);
 
-	// *** Load shaders and textures ***
-	std::vector<uint8_t> spotTexture;
-	unsigned int width, height;
-	lodepng::decode(spotTexture, width, height, "../models/spot.png");
+    // *** Load shaders and textures ***
+    std::vector<uint8_t> spotTexture;
+    unsigned int width, height;
+    lodepng::decode(spotTexture, width, height, "../models/spot.png");
 
-	LambertianShader redLambertianShader(red);
-	PhongShader bluePlasticShader(blue, Eigen::Vector3f(1.f, 1.f, 1.f), 100.f);
-	LambertianShader aquaLambertianShader(aqua);
-	LambertianShader lavenderLambertianShader(lavender);
-	TexturedLambertianShader spotShader(&spotTexture, width, height);
-	MirrorShader mirrorShader;
-	TexCoordTestShader texCoordTestShader;
+    LambertianShader redLambertianShader(red);
+    PhongShader bluePlasticShader(blue, Eigen::Vector3f(1.f, 1.f, 1.f), 100.f);
+    LambertianShader aquaLambertianShader(aqua);
+    LambertianShader lavenderLambertianShader(lavender);
+    TexturedLambertianShader spotShader(&spotTexture, width, height);
+    MirrorShader mirrorShader;
+    TexCoordTestShader texCoordTestShader;
 
-	// *** Set up scene ***
-	Scene scene;
+    // *** Load shaders and textures ***
 
-	Model CharacterMesh1("../models/Spidey_Head.obj");
-	Model CharacterMesh2("../models/Spidey_Torso.obj");
-	Model CharacterMesh3("../models/Spidey_Arms.obj");
-	Model CharacterMesh4("../models/Spidey_legs3.obj");
-	Model CharacterMesh5("../models/Asgard_Bed.obj");
-	Model CharacterMesh6("../models/Bed_Palenquin.obj");
-	Model CharacterMesh7("../models/Asgard_Pillar.obj");
-	Model CharacterMesh8("../models/Asgard_Pillar.obj");
-	Model CharacterMesh9("../models/Asgard_Floor.obj");
+    std::vector<uint8_t> spideyHeadTex, spideyBodyTex, spideyEquipTex, spideyLegsTex;
+    std::vector<uint8_t> asgardTex, pillarTex, marbleTex, palenquinTex;
 
-	auto makeTranslation = [](float x, float y, float z) {
-		Eigen::Matrix4f T = Eigen::Matrix4f::Identity();
-		T(0, 3) = x;
-		T(1, 3) = y;
-		T(2, 3) = z;
-		return T;
-		};
+    unsigned w1, h1, w2, h2, w3, h3, w4, h4, w5, h5, w6, h6, w7, h7, w8, h8;
 
-	auto makeRotateY = [](float angle) {
-		Eigen::Matrix4f R = Eigen::Matrix4f::Identity();
-		float c = cos(angle);
-		float s = sin(angle);
+    lodepng::decode(spideyBodyTex, w1, h1, "../models/T_1036800_Body_D.png");
+    lodepng::decode(spideyEquipTex, w2, h2, "../models/T_1036800_Equip_01_D.png");
+    lodepng::decode(spideyHeadTex, w3, h3, "../models/T_1036800_Head_D.png");
 
-		R(0, 0) = c;  R(0, 2) = s;
-		R(2, 0) = -s; R(2, 2) = c;
+    lodepng::decode(asgardTex, w4, h4, "../models/T_AsgardC02BuildingIND001Aa_D.png");
+    lodepng::decode(pillarTex, w5, h5, "../models/T_AsgardE01Pillar006A_D.png");
+    lodepng::decode(marbleTex, w6, h6, "../models/Marble.png");
 
-		return R;
-		};
+    lodepng::decode(spideyLegsTex, w7, h7, "../models/T_1036800_Equip_02_D.png");
+    lodepng::decode(palenquinTex, w8, h8, "../models/DarkGold.png");
 
-	LambertianShader fallbackShader(red);
+    TexturedLambertianShader spideyHeadShader(&spideyHeadTex, w3, h3);
+    TexturedLambertianShader spideyBodyShader(&spideyBodyTex, w1, h1);
+    TexturedLambertianShader spideyEquipShader(&spideyEquipTex, w2, h2);
+    TexturedLambertianShader spideyLegsShader(&spideyLegsTex, w7, h7);
 
-	Eigen::Matrix4f baseTransform =
-		makeTranslation(-0.2f, -2.0f, 12.f) * makeRotateY(M_PI - 0.2f);
+    TexturedLambertianShader asgardShader(&asgardTex, w4, h4);
+    TexturedLambertianShader pillarShader(&pillarTex, w5, h5);
+    TexturedLambertianShader floorShader(&marbleTex, w6, h6);
+    TexturedLambertianShader palenquinShader(&palenquinTex, w8, h8);
 
-	scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh1, &fallbackShader, 4, baseTransform));
-	scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh2, &fallbackShader, 4, baseTransform));
-	scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh3, &fallbackShader, 4, baseTransform));
-	scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh4, &fallbackShader, 4, baseTransform));
+    // *** Set up scene ***
+    Scene scene;
 
-	scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh5, &fallbackShader, 4,
-		makeTranslation(-0.4f, -2.0f, 12.f) * makeRotateY(M_PI - 0.2f)));
+    Model CharacterMesh1("../models/Spidey_Head.obj");
+    Model CharacterMesh2("../models/Spidey_Torso.obj");
+    Model CharacterMesh3("../models/Spidey_Arms.obj");
+    Model CharacterMesh4("../models/Spidey_legs3.obj");
+    Model CharacterMesh5("../models/Asgard_Bed.obj");
+    Model CharacterMesh6("../models/Bed_Palenquin.obj");
+    Model CharacterMesh7("../models/Asgard_Pillar.obj");
+    Model CharacterMesh8("../models/Asgard_Pillar.obj");
+    Model CharacterMesh9("../models/Asgard_Floor.obj");
 
-	scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh6, &fallbackShader, 4,
-		makeTranslation(-1.0f, -2.4f, 12.f) * makeRotateY(M_PI - 0.2f)));
+    auto makeTranslation = [](float x, float y, float z) {
+        Eigen::Matrix4f T = Eigen::Matrix4f::Identity();
+        T(0, 3) = x;
+        T(1, 3) = y;
+        T(2, 3) = z;
+        return T;
+        };
 
-	scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh7, &fallbackShader, 4,
-		makeTranslation(-0.2f, -2.4f, 15.f) * makeRotateY(M_PI - 0.2f)));
+    auto makeRotateY = [](float angle) {
+        Eigen::Matrix4f R = Eigen::Matrix4f::Identity();
+        float c = cos(angle);
+        float s = sin(angle);
+        R(0, 0) = c;  R(0, 2) = s;
+        R(2, 0) = -s; R(2, 2) = c;
+        return R;
+        };
 
-	scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh8, &fallbackShader, 4,
-		makeTranslation(-7.0f, -3.0f, 10.f) * makeRotateY(M_PI - 0.2f)));
+    Eigen::Matrix4f baseTransform =
+        makeTranslation(-0.2f, -2.0f, 12.f) * makeRotateY(M_PI - 0.2f);
 
-	scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh9, &fallbackShader, 4,
-		makeTranslation(-0.4f, -2.4f, 12.f) * makeRotateY(M_PI - 0.2f)));
+    scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh1, &spideyHeadShader, 4, baseTransform));
+    scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh2, &spideyBodyShader, 4, baseTransform));
+    scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh3, &spideyEquipShader, 4, baseTransform));
+    scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh4, &spideyLegsShader, 4, baseTransform));
 
-	// *** Add lights to scene ***
-	Eigen::Vector3f ambientLight(.1f, .1f, .1f);
+    scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh5, &asgardShader, 4,
+        makeTranslation(-0.4f, -2.0f, 12.f) * makeRotateY(M_PI - 0.2f)));
 
-	std::vector<std::unique_ptr<Light>> lightSources;
-	lightSources.push_back(std::make_unique<PointLight>(Eigen::Vector3f(-1.f, 3.f, -1.f), 3.f * Eigen::Vector3f(1.f, 1.f, 1.f)));
-	lightSources.push_back(std::make_unique<DirectionalLight>(Eigen::Vector3f(0.f, -1.f, 1.f), .5f * Eigen::Vector3f(1.f, 1.f, 1.f)));
+    scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh6, &palenquinShader, 4,
+        makeTranslation(-1.0f, -2.4f, 12.f) * makeRotateY(M_PI - 0.2f)));
 
-	// *** Render the scene ***
+    scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh7, &pillarShader, 4,
+        makeTranslation(-0.2f, -2.4f, 15.f) * makeRotateY(M_PI - 0.2f)));
 
-	std::vector<unsigned int> scanlines(pixHeight);
-	for (int i = 0; i < pixHeight; ++i) scanlines[i] = i;
+    scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh8, &pillarShader, 4,
+        makeTranslation(-7.0f, -3.0f, 10.f) * makeRotateY(M_PI - 0.2f)));
 
-	if (config["shuffleScanlines"]) {
-		std::random_device rd;
-		std::mt19937 g(rd());
-		std::shuffle(scanlines.begin(), scanlines.end(), g);
-	}
+    scene.renderables.push_back(std::make_shared<BVHNode>(CharacterMesh9, &floorShader, 4,
+        makeTranslation(-0.4f, -2.4f, 12.f) * makeRotateY(M_PI - 0.2f)));
 
-	auto startTime = std::chrono::steady_clock::now();
+    // *** Add lights to scene ***
+    Eigen::Vector3f ambientLight(.1f, .1f, .1f);
+
+    std::vector<std::unique_ptr<Light>> lightSources;
+    lightSources.push_back(std::make_unique<PointLight>(Eigen::Vector3f(-1.f, 3.f, -1.f), 3.f * Eigen::Vector3f(1.f, 1.f, 1.f)));
+    lightSources.push_back(std::make_unique<DirectionalLight>(Eigen::Vector3f(0.f, -1.f, 1.f), .5f * Eigen::Vector3f(1.f, 1.f, 1.f)));
+
+    // *** Render the scene ***
+    std::vector<unsigned int> scanlines(pixHeight);
+    for (int i = 0; i < pixHeight; ++i) scanlines[i] = i;
+
+    if (config["shuffleScanlines"]) {
+        std::random_device rd;
+        std::mt19937 g(rd());
+        std::shuffle(scanlines.begin(), scanlines.end(), g);
+    }
+
+    auto startTime = std::chrono::steady_clock::now();
 
 #pragma omp parallel for
-	for (int y = 0; y < pixHeight; ++y) {
-		for (int x = 0; x < pixWidth; ++x) {
+    for (int y = 0; y < pixHeight; ++y) {
+        for (int x = 0; x < pixWidth; ++x) {
 
-			Ray ray = cam.getRay(x, scanlines[y]);
-			HitInfo hitInfo;
+            Ray ray = cam.getRay(x, scanlines[y]);
+            HitInfo hitInfo;
 
-			if (scene.intersect(ray, 1e-6f, 1e6f, hitInfo, VISIBLE_BITMASK)) {
+            if (scene.intersect(ray, 1e-6f, 1e6f, hitInfo, VISIBLE_BITMASK)) {
 
-				Eigen::Vector3f color = hitInfo.shader->getColor(
-					hitInfo, &scene,
-					lightSources, ambientLight,
-					0, config["maxBounces"]);
+                Eigen::Vector3f color = hitInfo.shader->getColor(
+                    hitInfo, &scene,
+                    lightSources, ambientLight,
+                    0, config["maxBounces"]);
 
-				color = color.cwiseMin(1.f);
+                color = color.cwiseMin(1.f);
 
-				int line = (pixHeight - scanlines[y]) - 1;
+                int line = (pixHeight - scanlines[y]) - 1;
 
-				outImage[(x + line * pixWidth) * nChannels + 0] = (uint8_t)(color.x() * 255);
-				outImage[(x + line * pixWidth) * nChannels + 1] = (uint8_t)(color.y() * 255);
-				outImage[(x + line * pixWidth) * nChannels + 2] = (uint8_t)(color.z() * 255);
-				outImage[(x + line * pixWidth) * nChannels + 3] = 255;
-			}
-			else {
+                outImage[(x + line * pixWidth) * nChannels + 0] = (uint8_t)(color.x() * 255);
+                outImage[(x + line * pixWidth) * nChannels + 1] = (uint8_t)(color.y() * 255);
+                outImage[(x + line * pixWidth) * nChannels + 2] = (uint8_t)(color.z() * 255);
+                outImage[(x + line * pixWidth) * nChannels + 3] = 255;
+            }
+            else {
 
-				int line = (pixHeight - scanlines[y]) - 1;
+                int line = (pixHeight - scanlines[y]) - 1;
 
-				outImage[(x + line * pixWidth) * nChannels + 0] = 0;
-				outImage[(x + line * pixWidth) * nChannels + 1] = 0;
-				outImage[(x + line * pixWidth) * nChannels + 2] = 0;
-				outImage[(x + line * pixWidth) * nChannels + 3] = 255;
-			}
-		}
-	}
+                outImage[(x + line * pixWidth) * nChannels + 0] = 0;
+                outImage[(x + line * pixWidth) * nChannels + 1] = 0;
+                outImage[(x + line * pixWidth) * nChannels + 2] = 0;
+                outImage[(x + line * pixWidth) * nChannels + 3] = 255;
+            }
+        }
+    }
 
-	auto renderTime = std::chrono::steady_clock::now() - startTime;
+    auto renderTime = std::chrono::steady_clock::now() - startTime;
+    std::cout << "Render duration " << std::chrono::duration_cast<std::chrono::milliseconds>(renderTime).count() * 1e-3f << " seconds." << std::endl;
 
-	std::cout << "Render duration " << std::chrono::duration_cast<std::chrono::milliseconds>(renderTime).count() * 1e-3f << " seconds." << std::endl;
+    // *** Save the output image ***
+    int errorCode;
+    errorCode = lodepng::encode(config["outputFilename"], outImage, pixWidth, pixHeight);
+    if (errorCode) {
+        std::cout << "lodepng error encoding image: " << lodepng_error_text(errorCode) << std::endl;
+        return errorCode;
+    }
 
-	// *** Save the output image ***
-	int errorCode;
-	errorCode = lodepng::encode(config["outputFilename"], outImage, pixWidth, pixHeight);
-	if (errorCode) {
-		std::cout << "lodepng error encoding image: " << lodepng_error_text(errorCode) << std::endl;
-		return errorCode;
-	}
-
-	return 0;
+    return 0;
 }
